cede4fa36f9421f251cdbde5a2512b75a048ecc6
chintan <chintan@gmail.com>
2025-04-12 08:52:47 +0530

    all

---
+++ 001_Basic/datatypes.py
@@ -0,0 +1,51 @@
+
+# a = 10
+# b = 10.23
+# c = 4j+5
+# d = "Tops tech"
+# e = [10,20,30,40,50,50]
+# f = (10,20,30,40,50,50)
+# g = {10,20,30,40,50,50}
+# h = {"India":"IN","USA":"US"}
+# i = True
+# j = None
+
+
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+# print(type(i))
+# print(type(j))
+
+
+# a = "10"
+# b = "20"
+
+# c = int(a)+int(b)
+
+# print(c)
+
+# a = 10
+# b = float(a)
+# print(b)
+
+# a = "1000"
+# b = int(a)
+# print(b)
+
+
+# a = int(input("Enter a : "))
+# b = int(input("Enter b : "))
+# print(a+b)
+
+
---
+++ 001_Basic/demo.py
@@ -0,0 +1,35 @@
+# print("Hello Python")
+
+# print('Hello Tops')
+
+# print(""" 
+#         Hello Python
+#         Hello Tops
+#         Hello World
+# """)
+
+
+# Hello \"Python"
+
+# print('Hello\b "Python"')
+# print("hello \"python\" ")
+
+
+# a = "sun rises in east"
+# print(a.split(" "))
+
+# name ="vatsal"
+# surname = "mehta"
+
+# a = "my name is {1} and surname is {0}".format(name,surname)
+# print(a)
+
+# a = f"my name is {name} and surname is {surname}"
+# print(a)
+
+
+# print("Python","Java","Php","Android",sep=" | ")
+
+   
+print("Hello",end="")
+print("Python")
