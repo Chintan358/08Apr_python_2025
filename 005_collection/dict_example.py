a27ade83170d61b093c6fc54164833a52e480e5f
chintan <chintan@gmail.com>
2025-05-01 08:59:29 +0530

    collection

---
+++ 005_collection/dict_example.py
@@ -0,0 +1,48 @@
+
+st = {
+    "name":"Harsh",
+    "email":"harsh@gmail.com",
+    "phone":"8574524152",
+    "name":"Vishal",
+    "address":{
+        "city":"surat",
+        "state":"gujarat"
+    }
+}
+
+
+
+# print(st['email'])
+# print(st.get('email'))
+# print(st.keys())
+# print(st.values())
+
+# st['name']="Pratibha"
+# st['age'] = 25
+# print(st.items())
+
+# st.update({"age":"30"})
+# st.popitem()
+# del st['name']
+# st.clear()
+# print(st)
+
+# for i in st:
+#     print(i)
+
+# for i in st.keys():
+#     print(i)
+
+# for i in st.values():
+#     print(i)
+
+# for i,j in st.items():
+#     print(i,j)
+
+
+# print(st['address']['city'])
+
+# l = [10,20,30,40,50,{"city":"surat","state":"Gujarat"}]
+# print(l[5]['city'])
+
+
---
+++ 005_collection/set_examples.py
@@ -0,0 +1,45 @@
+s = {10,20,30,40,50,20,1,1,True,False,0}
+
+# print(s)
+
+# for i in s:
+#     print(i)
+
+# print(10 not in s)
+
+# a = {10,20,30}
+# b = {"a","b","c"}
+
+# a.add(100)
+# a.update(b)
+# a.update("Hello")
+
+# a.remove(100)
+# a.discard(100)
+# a.pop()
+# print(a)
+
+
+# a = {10,20,30,40,50,"a"}
+# b = {40,50,60,70,80}
+
+# c = a.union(b)
+# a.update(b)
+
+# c = a.intersection(b)
+# a.intersection_update(b)
+
+# c = a.difference(b)
+# a.difference_update(b)
+
+# c = a.symmetric_difference(b)
+# a.symmetric_difference_update(b)
+# print(a)
+
+
+a = {10,20}
+b = {40,50,10,20}
+
+# print(a.isdisjoint(b))
+# print(a.issubset(b))
+print(b.issuperset(a))
