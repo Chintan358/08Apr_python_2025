3e2a3c8366e0c87bf52d39b178ef04c7e2b08e22
chintan <chintan@gmail.com>
2025-06-28 09:56:28 +0530

    all

--- 014_Django/myapp/models.py
+++ 014_Django/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.CharField(max_length=22)
+    age = models.IntegerField()
